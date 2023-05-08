371b4e0c29a6b085ad0e4fcc8cdba50d32c37ddc
ricardop <rpajaro@sinprol.com.mx>
2023-05-08 15:43:09 -0600

    add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,14 @@ import 'package:chat_app/ui/screens/login_screen/login_screen.dart';
 import 'package:chat_app/ui/screens/register_screen/register_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const ProviderScope(child: App()));
 }
 
